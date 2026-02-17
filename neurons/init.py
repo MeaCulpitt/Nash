6fd8e3ff46251b0c7e7a5541b5f80134af712b01
MeaCulpitt <lculpitt@gmail.com>
2026-02-17 12:18:03 +1030

    Create init.py

---
+++ neurons/init.py
@@ -0,0 +1,12 @@
+**neurons/__init__.py**
+
+```python
+"""NASH Neurons - Miner and Validator entry points."""
+
+from neurons.miner import NashMiner
+from neurons.validator import NashValidator
+
+__all__ = ["NashMiner", "NashValidator"]
+```
+
+---

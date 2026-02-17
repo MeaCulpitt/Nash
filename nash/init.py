38aabcaf3294fafba15b321e060852d617a0e778
MeaCulpitt <lculpitt@gmail.com>
2026-02-17 12:14:27 +1030

    Create init.py

---
+++ nash/init.py
@@ -0,0 +1,17 @@
+**nash/__init__.py**
+
+```python
+"""NASH - Inter-Subnet Settlement Layer for Bittensor."""
+
+__version__ = "0.1.0"
+
+from nash.protocol import NashSynapse
+from nash.miner import NashMiner
+from nash.validator import NashValidator
+
+__all__ = ["NashSynapse", "NashMiner", "NashValidator"]
+```
+
+---
+
+Ready for the next file?
